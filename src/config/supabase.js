c5a244f1e3fe6682ca8a403c1afacc9e683bffa1
Malak Sobhy <167289328+Malkah04@users.noreply.github.com>
2026-02-02 16:54:35 +0200

    Update supabase.js

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,7 +1,9 @@
-import { createClient } from "@supabase/supabase-js";
+const { createClient } = require("@supabase/supabase-js");
 
 // used for upload images
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SECRET_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+module.exports = supabase;
