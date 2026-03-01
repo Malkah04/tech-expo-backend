34aa25154de7d4a8fa976037bae66a8e35fe3db3
Ahmed Moataz <54457157+AhmedMoataz485@users.noreply.github.com>
2026-03-01 04:10:21 +0200

    Delete src directory

--- src/config/providers.config.js
+++
@@ -1,5 +0,0 @@
-// Central list of supported OAuth providers.
-// Keep this in sync with frontend UI and passport strategies.
-const ALLOWED_PROVIDERS = ["google", "github"];
-
-module.exports = { ALLOWED_PROVIDERS };
--- src/utils/host.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-const host =
-  process.env.NODE_ENV === "production"
-    ? "https://www.techexpo.site"
-    : "http://localhost:3000";
-
-module.exports = host;
